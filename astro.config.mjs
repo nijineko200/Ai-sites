b12837f598f392e8b7e7bb4b40df4b031650930f
nijineko200 <q1316390@gmail.com>
2026-02-20 14:56:25 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,1 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
-export default defineConfig({
-  site: 'https://techpulse-global.pages.dev',
-  output: 'static',
-  // CSSの崩れを防ぐための設定
-  integrations: [], 
-});
